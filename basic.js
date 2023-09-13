a10e6c365ce25abb6e62ebda5c991af9c04c576a
WD Ronju <github.ronju@gmail.com>
2023-09-13 12:51:28 +0600

    first class of js

---
+++ basic.js
@@ -0,0 +1,22 @@
+var greeting = "Hello!";
+var  name =  "Ronju islam";
+var  welcome = console.log(greeting, name);
+console.log(welcome);
+
+var num1  =55;
+var num2 = 100;
+
+var addition2Num = num1 +  num2;
+console.log(addition2Num);  //Addition assignment
+
+var  substractionNum = num2 - num1;
+console.log(substractionNum); //Subtaction assignment
+
+var multiplicationNu = num1 *  num2;
+console.log(multiplicationNu); //multiplication  assignment
+
+var  divisionNum = num2 /num1;
+console.log(divisionNum); //division assignment
+
+var modulasNum = num1 % num2;
+console.log(modulasNum);
